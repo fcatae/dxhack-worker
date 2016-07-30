0e8fed4634dbdac90e828c329e9a617eb193f5d2
Fabricio Catae <fabricio.catae@microsoft.com>
2016-07-30 09:44:27 -0700

    read webpage

--- src/CrawlerWorker/Program.cs
+++ src/CrawlerWorker/Program.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
@@ -23,6 +24,11 @@ namespace CrawlerWorker
 
             string connectionString = Configuration["Database"];
 
+            DoWork(connectionString);
+        }
+
+        static void DoWork(string connectionString)
+        {
             List<string> linkList = new List<string>();
 
             using (SqlConnection conn = new SqlConnection(connectionString))
@@ -43,18 +49,40 @@ namespace CrawlerWorker
 
                     FeedSearcher(body);
                 }
-                
+
             }
         }
 
         static string LoadWebPage(string link)
         {
-            return "<html></html>";
+            string result = null;
+
+            using (var client = new HttpClient())
+            {
+                var url = new Uri(link);
+                var message = new HttpRequestMessage(HttpMethod.Get, url);
+                message.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko");
+                
+                HttpResponseMessage response = client.SendAsync(message).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    result = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+
+            return result;
         }
 
         static void FeedSearcher(string body)
         {
             Debug.WriteLine(body);
+            //using (var client = new HttpClient())
+            //{
+            //    var url = new Uri("");
+
+            //    client.PostAsync()
+            //}
         }
 
         static void InsertLink(SqlConnection conn)
